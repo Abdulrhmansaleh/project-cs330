cb672dfa9b2959a0fb922f5b38ae7a096659572e
Abdulrhmansaleh <abdulrhmansaleh20@gmail.com>
2024-05-08 18:27:55 +0300

    project done

--- TCP_CLIENT.java
+++ TCP_CLIENT.java
@@ -4,17 +4,37 @@ import java.util.*;
 public class TCP_CLIENT {
     public static void main(String[]args) throws Exception{
         
-        Scanner convert_to = new Scanner(System.in);
-
-        System.out.println("B: to convert to binary\n H: to convert to hexadecimal\n Q: to quit the client program");
-             
-        
-        
-        
         
         //client socket for sending data to a server
-      //  Socket client_Socket = new Socket("Vipin-PC",1014);
-    
+        Socket client_Socket = new Socket("Vipin-PC",1014);
+        
+        //to send and recieve data from client to a server and vice versa
+        DataInputStream recive = new DataInputStream(client_Socket.getInputStream());
+        DataOutputStream send = new DataOutputStream(client_Socket.getOutputStream());
+
+        //To get inputs from client
+        Scanner inputs = new Scanner(System.in);
+        
+        System.out.println("choose option\nB: to convert to binary\nH: to convert to hexadecimal\nQ: to quit the client program\n");
+
+        char option = inputs.next().charAt(0); //this variable for storing the answer of user option
+
+        if (option == 'Q') //if the option is quite then exit terminate the connection
+        {
+           client_Socket.close();
+           System.exit(0); 
+        }        
+
+        System.out.println("\nEnter A Number\n");
+
+        int number = inputs.nextInt(); //to get the user input
+        
+        send.writeChar(option);
+        send.writeInt(number);
+
+        System.out.println("the number is: "+recive.readUTF());
+        
+        send.close();recive.close();client_Socket.close();       
         
     
     }
--- TCP_SERVER.java
+++ TCP_SERVER.java
@@ -5,17 +5,22 @@ public class TCP_SERVER {
     public static void main(String[] args) throws Exception
     {
         // this variable for listening to the client that creates a socket
+        
         try
         {
-            ServerSocket listener = new ServerSocket(1014);
-            
+            ServerSocket listener = new ServerSocket(1014);            
             Socket response =  listener.accept(); //the listener socket waits until the client establish a tcp connection
+            //to send and recieve data from client to a server and vice versa
+            DataInputStream recive = new DataInputStream(response.getInputStream());
+            DataOutputStream send = new DataOutputStream(response.getOutputStream());
             
+            send.writeUTF(Integer.toBinaryString(recive.readInt()));
+            response.close();
         }catch(Exception exception){
-                System.out.println("Server is down, please try later.");
+            System.out.println("Server is down, please try later.");
         }
-
-
+    
+        
     } 
 
 
