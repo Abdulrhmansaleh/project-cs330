3b55e80a1fbe5daff3a315a5b5dcb95bc47dbc22
Abdulrhmansaleh <abdulrhmansaleh20@gmail.com>
2024-05-08 17:29:02 +0300

    client options

--- TCP_CLIENT.java
+++ TCP_CLIENT.java
@@ -1,3 +1,21 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
 public class TCP_CLIENT {
+    public static void main(String[]args) throws Exception{
+        
+        Scanner convert_to = new Scanner(System.in);
+
+        System.out.println("B: to convert to binary\n H: to convert to hexadecimal\n Q: to quit the client program");
+             
+        
+        
+        
+        
+        //client socket for sending data to a server
+      //  Socket client_Socket = new Socket("Vipin-PC",1014);
     
+        
+    
+    }
 }
--- TCP_SERVER.java
+++ TCP_SERVER.java
@@ -1,3 +1,22 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
 public class TCP_SERVER {
-    
+    public static void main(String[] args) throws Exception
+    {
+        // this variable for listening to the client that creates a socket
+        try
+        {
+            ServerSocket listener = new ServerSocket(1014);
+            
+            Socket response =  listener.accept(); //the listener socket waits until the client establish a tcp connection
+            
+        }catch(Exception exception){
+                System.out.println("Server is down, please try later.");
+        }
+
+
+    } 
+
+
 }
