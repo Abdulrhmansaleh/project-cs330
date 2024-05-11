d9707866d3a1ffc32a8136719345755c7396e585
Osamah <osamah.saddeq@gmail.com>
2024-05-11 18:17:56 +0300

    Added exception handling if server is down, and edited the error messages.

--- TCP_CLIENT.java
+++ TCP_CLIENT.java
@@ -1,59 +1,74 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+
+/**
+ * This class represents a TCP client that connects to a server and sends
+ * requests to convert numbers.
+ */
 public class TCP_CLIENT {
-    private static final int port = 1014;
-    private static String message = "";
-    
-    public static void main(String[]args) throws Exception{
-        
-        
-        //client socket for sending data to a server
-        Socket client_Socket = new Socket("127.0.0.1",port);
-        
-        //to send and recieve data from client to a server and vice versa
-        DataInputStream receive = new DataInputStream(client_Socket.getInputStream());
-        DataOutputStream send = new DataOutputStream(client_Socket.getOutputStream());
+  private static final int port = 1014;
+  private static String message = "";
 
-        //To get inputs from a client
-        Scanner inputs = new Scanner(System.in);
-        
-        System.out.println("choose option\nB: to convert to binary\nH: to convert to hexadecimal\nQ: to quit the client program\n");
-  
+  /**
+   * The main method of the TCP_CLIENT class.
+   * It establishes a connection with the server, sends requests, and receives
+   * responses.
+   */
+  public static void main(String[] args) {
 
-        String option = inputs.nextLine();//this variable for storing the answer of user option
-        
-        send.writeUTF(option); //send the option to TCP_SERVER
-        if (option.equalsIgnoreCase("Q")) {
-          send.close();receive.close();client_Socket.close(); 
-          System.exit(0);
-          
-        }
-        
-        System.out.print("\nEnter A Number: ");
-        
-        String number = inputs.nextLine(); //to get the decimal input
-        inputs.close();
-        
-        send.writeUTF(number);  //send the number to TCP_SERVER
-        
-        //display error messages received from TCP_SERVER
-        message = receive.readUTF();
-        if(!message.equals("200\tOk and the number is based on the request(B/H)"))
-        {
-          System.out.println(message);
-          send.close();receive.close();client_Socket.close(); 
-          System.exit(1);
-        
-        }
-        
-      
-       
-      
-        System.out.println("the number is: "+receive.readUTF()); //receive the converted number from TCP_SERVER
-        System.out.println(message + '\n');
-        send.close();receive.close();client_Socket.close();       
-        
-    
+    try {
+      // client socket for sending data to a server
+      Socket client_Socket = new Socket("127.0.0.1", port);
+
+      // to send and receive data from client to a server and vice versa
+      DataInputStream receive = new DataInputStream(client_Socket.getInputStream());
+      DataOutputStream send = new DataOutputStream(client_Socket.getOutputStream());
+
+      // To get inputs from a client
+      Scanner inputs = new Scanner(System.in);
+
+      System.out.println(
+          "choose option\nB: to convert to binary\nH: to convert to hexadecimal\nQ: to quit the client program\n");
+
+      String option = inputs.nextLine();// this variable for storing the answer of user option
+
+      send.writeUTF(option); // send the option to TCP_SERVER
+      if (option.equalsIgnoreCase("Q")) {
+        send.close();
+        receive.close();
+        client_Socket.close();
+        System.exit(0);
+
+      }
+
+      System.out.print("\nEnter A Number: ");
+
+      String number = inputs.nextLine(); // to get the decimal input
+      inputs.close();
+
+      send.writeUTF(number); // send the number to TCP_SERVER
+
+      // display error messages received from TCP_SERVER
+      message = receive.readUTF();
+      if (!message.equals("200ok\t")) {
+        System.out.print(message);
+        send.close();
+        receive.close();
+        client_Socket.close();
+        System.exit(1);
+
+      }
+
+      System.out.print(message);
+      System.out.println(receive.readUTF()); // receive the converted number from TCP_SERVER
+      send.close();
+      receive.close();
+      client_Socket.close();
+
+    } catch (Exception e) {
+      System.err.println("Server is down, please try again later.");
+      System.exit(0);
     }
+  }
 }
--- TCP_SERVER.java
+++ TCP_SERVER.java
@@ -1,121 +1,115 @@
 import java.io.*;
 import java.net.*;
+
 public class TCP_SERVER {
-    
-   private static int decimal = 0;
-   
-   private static String option;
-   
-   private static final int port = 1014;
-   
-   private static String message = "";
 
-   private static ServerSocket listener;
+  private static int decimal = 0;
 
-   private static Socket response; 
+  private static String option;
 
-   private static  DataInputStream receive;
+  private static final int port = 1014;
 
-   private static  DataOutputStream send;
+  private static String message = "";
 
-  
-  
-    public static void main(String[] args) 
-    {
-        
-       while(true) {
-        
-       
-        try
-        {
-            listener = new ServerSocket(port); // this variable for listening to the client that creates a socket            
-            response =  listener.accept(); //the listener socket waits until the client establish a tcp connection
-            
-            //to send and receive data from client to a server and vice versa
-             receive = new DataInputStream(response.getInputStream());
-             send = new DataOutputStream(response.getOutputStream());
-           
-            }catch(Exception exception){
-               
-               /////here is the server down message must goes to client class
-            }
-            try {
+  private static ServerSocket listener;
 
-                option = receive.readUTF();
-                
-               //to ensure 100% that it's a number and not letter or empty
-               try{
-               decimal = Integer.parseInt(receive.readUTF());
-               }
-               catch(Exception e){
-   
-               //if the option and the number missing display following message
-                if (!option.equalsIgnoreCase("B")&&!option.equalsIgnoreCase("H")&&!option.equalsIgnoreCase("H"))
-                {
-                 message =  "500\tMissing both the letter and number";
-                 send.writeUTF(message);
-                 response.close();listener.close();
-                }
-                else{
-   
-                   message =  "400\tMissing the number";
-                   send.writeUTF(message);
-                   response.close();listener.close();
-                }
-               }
-               
-               //if the missing value was the option
-               if(!option.equalsIgnoreCase("B")&&!option.equalsIgnoreCase("H")&&!option.equalsIgnoreCase("Q")){
-                  message = "300\tMissing B or H";
-                  send.writeUTF(message);
-                  response.close();listener.close();
-               }
-               else if(option.equalsIgnoreCase("B")){
-                message = "200\tOk and the number is based on the request(B/H)";
-                send.writeUTF(message);
-                send.writeUTF(Integer.toBinaryString(decimal));
-               } 
-               else if(option.equalsIgnoreCase("H")){
-                message = "200\tOk and the number is based on the request(B/H)";
-                send.writeUTF(message);
-                send.writeUTF(Integer.toHexString(decimal));
-               }
-               //here to close TCP connection in case of quit option
-                else{
-                    response.close();listener.close();
-               }
+  private static Socket response;
 
-             response.close();listener.close(); //close tcp connection
-            }catch(Exception e){
-             ///////////////////////////////// Empty
-            }
+  private static DataInputStream receive;
 
-        
-       }
-    
-        
-    } 
-    
+  private static DataOutputStream send;
+
+  public static void main(String[] args) {
+
+    while (true) {
+
+      try {
+        listener = new ServerSocket(port); // this variable for listening to the client that creates a socket
+        response = listener.accept(); // the listener socket waits until the client establish a tcp connection
+
+        // to send and receive data from client to a server and vice versa
+        receive = new DataInputStream(response.getInputStream());
+        send = new DataOutputStream(response.getOutputStream());
+
+      } catch (Exception exception) {
+
+        ///// here is the server down message must goes to client class
+      }
+      try {
+
+        option = receive.readUTF();
+
+        // to ensure 100% that it's a number and not letter or empty
+        try {
+          decimal = Integer.parseInt(receive.readUTF());
+        } catch (Exception e) {
+
+          // if the option and the number missing display following message
+          if (!option.equalsIgnoreCase("B") && !option.equalsIgnoreCase("H") && !option.equalsIgnoreCase("H")) {
+            message = "500\tRequest is empty";
+            send.writeUTF(message);
+            response.close();
+            listener.close();
+          } else {
+
+            message = "400\tThe number is missing";
+            send.writeUTF(message);
+            response.close();
+            listener.close();
+          }
+        }
+
+        // if the missing value was the option
+        if (!option.equalsIgnoreCase("B") && !option.equalsIgnoreCase("H") && !option.equalsIgnoreCase("Q")) {
+          message = "300\tBad request";
+          send.writeUTF(message);
+          response.close();
+          listener.close();
+        } else if (option.equalsIgnoreCase("B")) {
+          message = "200ok\t";
+          send.writeUTF(message);
+          send.writeUTF(Integer.toBinaryString(decimal));
+        } else if (option.equalsIgnoreCase("H")) {
+          message = "200ok\t";
+          send.writeUTF(message);
+          send.writeUTF(Integer.toHexString(decimal).toUpperCase());
+        }
+        // here to close TCP connection in case of quit option
+        else {
+          response.close();
+          listener.close();
+        }
+
+        response.close();
+        listener.close(); // close tcp connection
+      } catch (Exception e) {
+        ///////////////////////////////// Empty
+      }
+
+    }
+
+  }
 
 }
 
-/*    
-  char option = recive.readChar();
-          boolean is_int = true;
-try{
-     
-      decimal = Integer.parseInt(recive.readUTF());
-     System.out.println(decimal+"ok");
-  }catch(Exception e )
-{
-  is_int = false;
-  if (!is_int && (option !='H' || option != 'B'))
-  {
-      //send error message
-      
-
-      response.close();
-      System.exit(0);
-  }
-    
-} */
+/*
+ * char option = recive.readChar();
+ * boolean is_int = true;
+ * try{
+ * 
+ * decimal = Integer.parseInt(recive.readUTF());
+ * System.out.println(decimal+"ok");
+ * }catch(Exception e )
+ * {
+ * is_int = false;
+ * if (!is_int && (option !='H' || option != 'B'))
+ * {
+ * //send error message
+ * 
+ * 
+ * response.close();
+ * System.exit(0);
+ * }
+ * 
+ * }
+ */
